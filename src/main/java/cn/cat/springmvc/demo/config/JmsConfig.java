8f99e2fe0896a1c181d050ff673f69ecb4819457
cat <caihaitaoai@live.com>
2018-01-29 20:46:51 +0800

    Java配置分离

---
+++ src/main/java/cn/cat/springmvc/demo/config/JmsConfig.java
@@ -0,0 +1,10 @@
+package cn.cat.springmvc.demo.config;
+
+/**
+ * @Author: cat
+ * @Date: Created in 2018/1/29
+ * @PersonHome: http://blog.csdn.net/csdn6497
+ * @Description:
+ */
+public class JmsConfig {
+}

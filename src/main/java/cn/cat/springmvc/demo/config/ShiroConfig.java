bb178f3b0e1113cfa62fadc9354879b991d21020
cat <caihaitaoai@live.com>
2018-01-30 17:32:03 +0800

    Java配置分离

---
+++ src/main/java/cn/cat/springmvc/demo/config/ShiroConfig.java
@@ -0,0 +1,58 @@
+package cn.cat.springmvc.demo.config;
+
+import cn.cat.springmvc.demo.shiro.CustomCredentialsMatcher;
+import cn.cat.springmvc.demo.shiro.MyRealm;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @Author: cat
+ * @Date: Created in 2018/1/30
+ * @PersonHome: http://blog.csdn.net/csdn6497
+ * @Description:
+ */
+
+public class ShiroConfig implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
+        delegatingFilterProxy.setTargetFilterLifecycle(true);
+        FilterRegistration.Dynamic filter = servletContext.addFilter("delegatingFilter", delegatingFilterProxy);
+        filter.addMappingForUrlPatterns(null, false, "/*");
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter() {
+        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+
+        return null;
+    }
+
+    @Bean
+    public DefaultWebSecurityManager securityManager() {
+        return null;
+    }
+
+    @Bean
+    public AuthorizingRealm realm(CredentialsMatcher credentialsMatcher) {
+        AuthorizingRealm realm = new MyRealm();
+        realm.setCredentialsMatcher(credentialsMatcher);
+        return null;
+    }
+
+    @Bean
+    public CredentialsMatcher credentialsMatcher() {
+        return new CustomCredentialsMatcher();
+    }
+
+}
